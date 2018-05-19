4d2424436ce7528c5250fc2257938557485fdaa7
birsingh <birinder.singh@gmail.com>
2018-05-19 13:24:49 +0530

    Added Launch.json and changes to server.js file

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 "use strict";
 
 var http = require('http');
+var url  = require('url');
 
-var staticServe = function(req, res) { 
-    console.log("Request listener called of createServer"); 
-    res.statusCode = 200;
+var requestCallback = function(req, res) { 
+    console.log("Callback called when user connected to server"); 
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var urlObject = url.parse(req.url, true)
+    res.write(urlObject.query);
+    console.log(req.url);
     res.write('ok');
     return res.end();
 };
 
-var httpServer = http.createServer(staticServe);
+var httpServer = http.createServer(requestCallback);
 
 httpServer.listen(8080,function(){
     console.log('[INFO] httpServer Listening on *:' + 8080 +"port");
